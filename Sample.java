ad413b1dd7dae5e64ca30bf626e4fb88b4ff7053
SOFTECHSOLUTION <krushna1308@gmail.com>
2023-04-24 21:20:25 +0530

    Department class is Deleted

--- Sample.java
+++ Sample.java
@@ -4,37 +4,4 @@ class Sample
 		
 	}
 }
-import java.util.*;
-class Department
-{
-	String name;
-	int student;
-	double per;
-	Scanner sc=new Scanner(System.in);
-	Department()
-	{
-		System.out.println("Enter For First Department");
-		System.out.println("Enter Name of HOD");
-		name=sc.nextLine();
-		System.out.println("Enter Total Students");
-		student=sc.nextInt();
-		System.out.println("Enter the Average Result");
-		per=sc.nextDouble();
-	}
-	Department(String name)
-	{
-		System.out.println("Enter For Second Department");
-		this.name=name;
-		System.out.println("Enter Total Students");
-		student=sc.nextInt();
-		System.out.println("Enter the Average Result");
-		per=sc.nextDouble();
-	}
-	void display()
-	{
 
-		System.out.println("Name of HOD: "+name);
-		System.out.println("Total Students: "+student);
-		System.out.println("Average Result: "+per);
-	}
-}
